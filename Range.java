fbbe739efa45444c61d92f6f055378a679d42d05
Ravi253 <rkchandu253@gmail.com>
2016-09-17 21:00:52 +0530

    Create Range.java

---
+++ Range.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+class Range 
+{
+	
+	public static void main(String[] args) 
+	{
+        int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first positive Two digit number");
+	      a=sc.nextInt();
+		 System.out.println("Enter second positive Two digit number");
+		   b=sc.nextInt();
+		 
+		if(a>=10 && a<=20){
+			if(b>=10 && b<=20)
+				  System.out.println("true");
+			else
+			     System.out.println("false");
+		}
+		else
+			     System.out.println("false");
+		}
+
+
+
+	}
+
