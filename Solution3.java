21a2d9f0a260b540b925b52821898f5d5d7dddbb
Ravi253 <rkchandu253@gmail.com>
2016-09-20 12:23:01 +0530

    Create Solution3.java

---
+++ Solution3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class  Solution3
+{
+		public void same(String s){
+			int count=0;
+
+			char c[]=s.toCharArray();
+			for (int i = 0 ; i!= c.length-2 ; i++) {
+				
+				  
+				if(c[i]==c[i+1]){
+					if(c[i+1]==c[i+2])
+					count++;
+				}
+				}
+		  
+		   if(count<=3){
+		   System.out.println(count);
+
+		   }
+		   else
+			System.out.println(0);
+	
+		
+		}
+	public static void main(String[] args) 
+	{
+		Solution3 s=new Solution3();
+		s.same("aaaabbjjccc");
+
+	}
+}
